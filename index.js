059a085408aafed5c7a1cef6de5efa2faa7ed29f
Albert Martin <albert@imor.tl>
2016-12-30 12:06:31 -0500

    feat(plugins): add plugin loading from support directory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Nautilus = require('nautilus/core');
 const electron = require('electron');
+const path = require('path');
 
 /**
  * Nautilus Desktop provides the same convention over configuration pattern for
@@ -22,6 +23,22 @@ class NautilusDesktop extends Nautilus {
     this.loadHooks('custom');
 
     this.loadHooks('custom', 'channels');
+
+    this.loadPlugins();
+  }
+
+  /**
+   * Plugins are loaded from the preferences directory. This changes based on
+   * the operating system, but under macOS will be located at
+   * `~/Library/Application Support/[app]/Plugins`. Plugins should be located
+   * in their own directory with a `package.json` present for version info.
+   * Currently plugins are executed with the entire application object which
+   * means they have full permission and could be potentially destructive.
+   */
+  loadPlugins() {
+    let plugins = path.dirname(this.app.preferences.path);
+    plugins = path.resolve(plugins, 'Plugins');
+    this.loadHooks('user', plugins);
   }
 }
 
